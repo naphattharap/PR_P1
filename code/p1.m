57177e658b7f8382d385560a7559786053e6b89e
naphatthara <naphatthara@naphattharasmbp.home>
2018-10-20 15:50:15 +0200

    test commit

--- code/p1.m
+++ code/p1.m
@@ -23,8 +23,6 @@ emotionsUsed = [0 1 3 4 5 6 7];
 % and labels from database under folder name CKDB
 [imagesData shapeData labels] = extractData('../CKDB', emotionsUsed);
 
-
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%% DIVIDE DATA (TRAIN/TEST) WITH CROSS VALIDATION  %%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
